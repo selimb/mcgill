e3104cf4876751071454d6117799c60be62c0014
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-09-16 21:09:52 -0400

    Use modules

--- quasi1d/qod.f
+++ quasi1d/qod.f
@@ -1,26 +1,45 @@
-        subroutine mkgrid(nx, x, s)
-C       Make initial grid.        
-           integer, intent(in) :: nx
-           real*8, dimension(nx), intent(out) :: x, s
-           real*8 :: h, t1, t2, pi, dx
-           pi = 4*atan(1.0_8)
-           h = 0.025
-           t1 = 0.8
-           t2 = 3
-           dx = 1.0/nx
-           write(*,*) nx
-           write(*,*) pi
-           do i=1, nx
+        module constants
+            implicit none
+C           Math
+            real*8, public, parameter :: pi = 4*atan(1.0_8)
+C           Grid constants
+            real*8, public, parameter :: h = 0.025
+            real*8, public, parameter :: t1 = 0.8
+            real*8, public, parameter :: t2 = 3
+C           Fluid constants
+            real*8, public, parameter :: g = 1.4
+            real*8, public, parameter :: R = 1716
+C           BCs
+            real*8, public, parameter :: ttot_in = 530.2
+            real*8, public, parameter :: ptot_in = 2117.0
+            real*8, public, parameter :: M_in = 1.2
+        end module
+        module input
+            implicit none
+            integer, public, parameter :: nx = 40
+            real*8, public, parameter :: dx = 1.0/nx
+        end module
+C       Make initial grid.
+        subroutine mkgrid(x, s)
+            use input
+            implicit none
+            real*8, dimension(nx), intent(out) :: x, s
+            integer :: i
+            do i=1, nx
                 x(i) = i*dx
                 s(i) = 1 - h*(sin(pi*x(i)**t1))**t2
-           end do
-           write(*,*) x(2)
-           write(*,*) s(2)
+            end do
         end
-
+C       Initialize field
+        subroutine init(w)
+            use constants
+            implicit none
+            real*8
+        end
         program quasi
-        implicit none
-
+            implicit none
+            use constants
+            use input
 C       1. INITIALIZE
 C           SETUP THE GRID
 C           INITIALIZE STATE VECTOR
@@ -28,26 +47,13 @@ C               - Density
 C               - Momentum
 C               - Energy
 C           IMPOSE EXIT STATIC PRESSURE
-
-        integer :: nx, i
-        real*8 :: dx, pi, g, t0t, p0t, R, M_in
-        real*8, dimension(:), allocatable :: x, s
-
-C       FLOW CONSTANTS
-        t0t = 531.2
-        g = 1.4
-        M_in = 1.2
-        R = 1716
-
-C       GENERATE GRID
-        nx = 40
-        allocate(x(nx), s(nx))
-        call mkgrid(nx, x, s)
-        open(3, file='bump3dgrid.p3dfmt',form='formatted')
-        write(3,*) (x(i),i=1,nx),
-     &             (s(i),i=1,nx)
-        deallocate (x, s)
-
+            integer :: i
+            real*8, dimension(nx) :: x, s
+            call mkgrid(x, s)
+            call init
+            open(3, file='bump3dgrid.p3dfmt',form='formatted')
+            write(3,*) (x(i),i=1,nx),
+       &               (s(i),i=1,nx)
 C       2. ITERATION LOOP
 
 
