fd11f5bc406046a9ebedab309e8b0e14541412e5
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-09-28 11:27:38 -0400

    Re-insert grid definition

--- quasi1d/main.f
+++ quasi1d/main.f
@@ -1,4 +1,18 @@
 C       ===============================================================
+C       Note on implementation.
+C           We pass around `prim` between functions. This
+C           corresponds
+C           to (rho, u, p e, c), the primitive variables.
+C           Generating W and
+C           F is easy, and the primitive variables are readily 
+C           available when necessary, such as when plotting or
+C           calculating the maximum eigenvalues. 
+C       Picture of grid indices:
+C             node        edge
+C           |---o---|---o---|---o ... ---|---o---|
+C               1   1   2   2   3       J-1  J
+C       ===============================================================
+C       ===============================================================
 C       Main
 C       ===============================================================
         program main
