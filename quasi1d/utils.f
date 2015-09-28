292d198fe0951f73f90a2505654eb02128275297
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-09-28 16:50:20 -0400

    utilities to write shit

--- quasi1d/utils.f
+++ quasi1d/utils.f
@@ -6,35 +6,23 @@ C       ===============================================================
         implicit none
         contains
 
-        subroutine writeall(w, f, q, f_edge, r)
-            real(dp), dimension(:, :), intent(in) :: w, f, q, f_edge, r
+        subroutine write1d(x)
+            real(dp), dimension(:), intent(in) :: x
             integer :: i, n
-            write(*, *) 'W1'
-            write(*, *) (w(1, i), i=1,n) 
-            write(*, *) 'W2'
-            write(*, *) (w(2, i), i=1,n) 
-            write(*, *) 'W3'
-            write(*, *) (w(3, i), i=1,n) 
-            write(*, *) 'F1'
-            write(*, *) (f(1, i), i=1,n) 
-            write(*, *) 'F2'
-            write(*, *) (f(2, i), i=1,n) 
-            write(*, *) 'F3'
-            write(*, *) (f(3, i), i=1,n) 
-            write(*, *) 'Q2'
-            write(*, *) (q(2, i), i=1,n)
-            write(*, *) 'Fedge1'
-            write(*, *) (f_edge(1, i), i=1,n) 
-            write(*, *) 'Fedge2'
-            write(*, *) (f_edge(2, i), i=1,n) 
-            write(*, *) 'Fedge3'
-            write(*, *) (f_edge(3, i), i=1,n) 
-            write(*, *) 'r1'
-            write(*, *) (r(1, i), i=1,n) 
-            write(*, *) 'r2'
-            write(*, *) (r(2, i), i=1,n) 
-            write(*, *) 'r3'
-            write(*, *) (r(3, i), i=1,n) 
+            n = size(x)
+            do i = 1, n
+                write(*,*) x(i)
+            end do
+        end subroutine
+
+        subroutine write2d(X)
+            real(dp), dimension(:, :), intent(in) :: X
+            integer :: i, n, k
+            n = size(X, 2)
+            do k = 1, 3
+                write(*,*) k
+                call write1d(X(k, :))
+            end do
             write(*, *) ''
             write(*, *) ''
             write(*, *) ''
