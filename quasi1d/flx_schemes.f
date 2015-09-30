e86208cdcebaf86e8bdeabdc22bc70744e4bd903
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-09-30 18:38:23 -0400

    working on roe. buggy for now

--- quasi1d/flx_schemes.f
+++ quasi1d/flx_schemes.f
@@ -14,20 +14,25 @@ C       ===============================================================
         use common_calcs
         implicit none
         contains
+
+        pure function calc_lambdas(u, c) result (lambdas)
+            real(dp), intent(in) :: u, c
+            real(dp), dimension(3) :: lambdas
+            lambdas(1) = u
+            lambdas(2) = u + c
+            lambdas(3) = u - c
+        end function
 C       Calculate the diagonalizers S^-1*C^-1 and CS
-        pure subroutine calc_diagonalizers(prim, SCinv, CS)
-            real(dp), dimension(:), intent(in) :: prim
+        pure subroutine calc_diagonalizers(rho, u, c, SCinv, CS)
+            real(dp), intent(in) :: rho, u, c
             real(dp), dimension(3, 3), intent(out) :: SCinv, CS
             real(dp), dimension(3, 3) :: S, CC, Sinv, Cinv
-            real(dp) :: beta, alpha, rho, u, c, c2
+            real(dp) :: beta, alpha, c2
             S(:, :) = 0
             Sinv(:, :) = 0
             CC(:, :) = 0
             Cinv(:, :) = 0
             beta = gam - 1
-            rho = prim(1)
-            u = prim(2)
-            c = prim(5)
             c2 = c**2
             alpha = 0.5*u**2
             S(1, 1) = 1
@@ -61,19 +66,15 @@ C       Calculate the diagonalizers S^-1*C^-1 and CS
 
 C       Calculate the diagonal matrices Yp and Ym according to the
 C       Steger Warming scheme
-        pure subroutine calc_diag_sw(prim, Yp, Ym)
-            real(dp), dimension(:), intent(in) :: prim
+        pure subroutine calc_diag_sw(u, c, Yp, Ym)
+            real(dp), intent(in) :: u, c
             real(dp), dimension(3, 3), intent(out) :: Yp, Ym
             real(dp), dimension(3) :: lambdas
-            real(dp) :: u, c, sq
+            real(dp) :: sq
             integer :: i
             Yp(:, :) = 0
             Ym(:, :) = 0
-            u = prim(2)
-            c = prim(5)
-            lambdas(1) = u
-            lambdas(2) = u + c
-            lambdas(3) = u - c
+            lambdas = calc_lambdas(u, c)
             do i = 1, 3
                 sq = sqrt(lambdas(i)**2 + params%eps**2)
                 Yp(i, i) = 0.5*(lambdas(i) + sq)
@@ -81,20 +82,39 @@ C       Steger Warming scheme
             end do
         end subroutine
 
+C       Calculate Jacobian from components
+        pure function calc_jacob(SCinv, Y, CS) result (A)
+            real(dp), dimension(3, 3), intent(in) :: SCinv, Y, CS
+            real(dp), dimension(3, 3) :: A
+            A = matmul( matmul(SCinv, Y), CS )
+        end function
+
+C       Calculate the jacobian for Steger-Warming schemes
+        pure subroutine calc_jacob_sw(prim, Ap, Am)
+            real(dp), dimension(:), intent(in) :: prim
+            real(dp), dimension(3, 3), intent(out) :: Ap, Am
+            real(dp), dimension(3, 3) :: Yp, Ym, CS, SCinv
+            real(dp) :: rho, u, c
+            rho = prim(1)
+            u = prim(2)
+            c = prim(5)
+            call calc_diagonalizers(rho, u, c, SCinv, CS)
+            call calc_diag_sw(u, c, Yp, Ym)
+            Ap = calc_jacob(SCinv, Yp, CS)
+            Am = calc_jacob(SCinv, Ym, CS)
+        end subroutine
+C       ---------------------------------------------------------------
 C       Steger-Warming
+C       ---------------------------------------------------------------
         pure function flx_sw(prim, w, f) result (f_edge)
             real(dp), dimension(:, :), intent(in) :: prim
             real(dp), dimension(3, size(prim, 2)), intent(in) :: w, f
             real(dp), dimension(3, size(prim, 2) - 1) :: f_edge
             real(dp), dimension(3, 3, size(prim, 2)) :: Ap, Am
-            real(dp), dimension(3, 3) :: Yp, Ym, CS, SCinv
             integer :: i, n
             n = size(prim, 2)
             do i = 1, n
-                call calc_diagonalizers(prim(:, i), SCinv, CS)
-                call calc_diag_sw(prim(:, i), Yp, Ym)
-                Ap(:, :, i) = matmul( matmul(SCinv, Yp), CS )
-                Am(:, :, i) = matmul( matmul(SCinv, Ym), CS )
+                call calc_jacob_sw(prim(:, i), Ap(:, :, i), Am(:, :, i))
             end do
             do i = 1, n - 1
                 f_edge(:, i) = matmul(Ap(:, :, i), w(:, i))
@@ -102,14 +122,15 @@ C       Steger-Warming
             end do
         end function flx_sw
 
+C       ---------------------------------------------------------------
 C       Modified Steger-Warming
+C       ---------------------------------------------------------------
         pure function flx_msw(prim, w, f) result (f_edge)
             real(dp), dimension(:, :), intent(in) :: prim
             real(dp), dimension(3, size(prim, 2)), intent(in) :: w, f
             real(dp), dimension(3, size(prim, 2) - 1) :: f_edge
             real(dp), dimension(3, 3) :: Ap_edge, Am_edge
             real(dp), dimension(5) :: prim_edge
-            real(dp), dimension(3, 3) :: Yp, Ym, CS, SCinv
             integer :: i, k, n, m
             n = size(prim, 2)
             m = size(prim, 1)
@@ -117,16 +138,15 @@ C       Modified Steger-Warming
                 do k = 1 , m
                     prim_edge(k) = 0.5*(prim(k, i) + prim(k, i + 1))
                 end do
-                call calc_diagonalizers(prim_edge, SCinv, CS)
-                call calc_diag_sw(prim_edge, Yp, Ym)
-                Ap_edge = matmul( matmul(SCinv, Yp), CS )
-                Am_edge = matmul( matmul(SCinv, Ym), CS )
+                call calc_jacob_sw(prim_edge, Ap_edge, Am_edge)
                 f_edge(:, i) = matmul(Ap_edge, w(:, i))
      &                         + matmul(Am_edge, w(:, i + 1))
             end do
         end function flx_msw
 
+C       ---------------------------------------------------------------
 C       Corrected-Modified Steger-Warming
+C       ---------------------------------------------------------------
         pure function flx_cmsw(prim, w, f) result(f_edge)
             real(dp), dimension(:, :), intent(in) :: prim
             real(dp), dimension(3, size(prim, 2)), intent(in) :: w, f
@@ -149,7 +169,63 @@ C       Corrected-Modified Steger-Warming
             end do
         end function flx_cmsw
 
+C       ---------------------------------------------------------------
+C       Roe
+C       ---------------------------------------------------------------
+        function flx_roe(prim, w, f) result (f_edge)
+            real(dp), dimension(:, :), intent(in) :: prim
+            real(dp), dimension(3, size(prim, 2)), intent(in) :: w, f
+            real(dp), dimension(3, size(prim, 2) - 1) :: f_edge
+            real(dp), dimension(3, 3) :: Y, CS, SCinv, A
+            real(dp) :: rho, rhoh, rhop, sqrho, sqrhop, u, up,
+     &          e, ep, p, pp, c, cp, ch, uh, hh, eps
+            real(dp), dimension(3) :: lam, lamp, lamh
+            integer :: i, k, n
+            n = size(prim, 2)
+            do i = 1, n - 1
+                rho = prim(1, i)
+                rhop = prim(1, i + 1)
+                sqrho = sqrt(rho)
+                sqrhop = sqrt(rhop)
+                u = prim(2, i)
+                up = prim(2, i + 1)
+                e = prim(5, i)
+                ep = prim(5, i + 1)
+                p = prim(3, i)
+                pp = prim(3, i + 1)
+                c = prim(5, i)
+                cp = prim(5, i + 1)
+                rhoh = sqrho*sqrhop
+                uh = (sqrho*u + sqrhop*up)/(sqrho + sqrhop)
+                hh = ( sqrho*((e + p)/rho) + sqrhop*((ep + pp)/rhop))
+     &               /(sqrho + sqrhop)
+C               TODO Maybe this is not squared
+C               ch = sqrt((gam - 1)*(hh - 0.5*uh**2))
+                ch = sqrt((gam - 1)*(hh - 0.5*uh*2))
+                call calc_diagonalizers(rhoh, uh, ch, SCinv, CS)
+C               Calculate eigenvalues
+                Y(:, :) = 0
+                lam = calc_lambdas(u, c)
+                lamp = calc_lambdas(up, cp)
+                lamh = calc_lambdas(uh, ch)
+                do k = 1, 3
+                    eps = max(0.0_dp, lamh(k)-lam(k), lamp(k)-lamh(k))
+                    if (abs(lamh(k)) <= eps) then
+                        lamh(k) = 0.5*(lamh(k)**2/eps + eps)
+                    end if
+                    Y(k, k) = lamh(k)
+                end do
+C               Calculate Jacobian and Flux
+                A = abs(calc_jacob(SCinv, Y, CS))
+                f_edge(:, i) = 0.5*(f(:, i) + f(:, i + 1)
+     &                           - matmul(A, w(:, i+1) - w(:, i))
+     &          )
+            end do
+        end function
+
+C       ---------------------------------------------------------------
 C       Scalar Dissipation
+C       ---------------------------------------------------------------
         pure function flx_scalar(prim, w, f) result (f_edge)
             real(dp), dimension(:, :), intent(in) :: prim
             real(dp), dimension(3, size(prim, 2)), intent(in) :: w, f
@@ -173,7 +249,8 @@ C       Choose a flux evaluation scheme based on input
 C       1 : Scalar dissipation
 C       2 : Steger Warming
 C       3 : Modified Steger Warming
-        pure function flx_eval(prim, w, f) result (f_edge)
+C       4 : Roe
+        function flx_eval(prim, w, f) result (f_edge)
             real(dp), dimension(:, :), intent(in) :: prim
             real(dp), dimension(3, size(prim, 2)), intent(in) :: w, f
             real(dp), dimension(3, size(prim, 2) - 1) :: f_edge
@@ -186,6 +263,8 @@ C       3 : Modified Steger Warming
                     f_edge = flx_msw(prim, w, f)
                 case (4)
                     f_edge = flx_cmsw(prim, w, f)
+                case (5)
+                    f_edge = flx_roe(prim, w, f)
                 case default
                     f_edge = flx_scalar(prim, w, f)
             end select
